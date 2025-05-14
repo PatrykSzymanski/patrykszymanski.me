ead9cd2d695a00552f6f1e0d2ac28a3694d6fb1d
PatrykSzymanski <54416174+PatrykSzymanski@users.noreply.github.com>
2025-05-14 20:02:34 +0200

    Test

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,4 @@
 module.exports = function(eleventyConfig) {
-  // Pass through specific files from src to _site
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/images");
@@ -7,14 +6,13 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/_redirects");
   eleventyConfig.addPassthroughCopy("src/_headers");
 
-  // Set the directory configuration
   return {
     dir: {
       input: "src",
       output: "_site",
       includes: "_includes"
     },
-    // Prevent duplicate processing
+    
     passthroughFileCopy: true
   };
 };
