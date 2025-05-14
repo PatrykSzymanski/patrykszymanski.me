c2f3da85d2f047210936f15ab0114092e904980f
PatrykSzymanski <54416174+PatrykSzymanski@users.noreply.github.com>
2025-05-14 16:27:48 +0200

    Fix deploy

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,8 @@
 module.exports = function(eleventyConfig) {
-  // Passthrough copy for static assets
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/favicon.*");
-  // Copy _headers file from src to output directory (_site)
-  eleventyConfig.addPassthroughCopy({ "src/_headers": "_headers" });
-  // Copy _routes.json file from root to output directory (_site)
-  eleventyConfig.addPassthroughCopy({ "_routes.json": "_routes.json" });
 
   return {
     dir: {
