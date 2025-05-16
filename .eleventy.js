d6b9c86fe85f9df08d46f90ce8800727cf2e3f33
PatrykSzymanski <54416174+PatrykSzymanski@users.noreply.github.com>
2025-05-16 11:02:01 +0200

    fix: content security policy

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/css/*.css");
+  eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/favicon.*");
